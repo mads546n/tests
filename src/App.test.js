407e20ab9f63d39cfdba3d2cd27cb2a6f71c5a46
Madsen <57358213+mads546n@users.noreply.github.com>
2025-09-04 16:50:33 +0200

    Add files via upload

---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+  let button = screen.getByRole("button");
+  expect(button).toHaveTextContent("Login");
+});
+test('has login-button', ()=>{
+  render(<App />);
+});
